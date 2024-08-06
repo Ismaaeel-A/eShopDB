8ddd87bd204c53f051a19f1c7b258363d22cb174
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-08-06 15:31:01 +0200

    backend

--- config/index.js
+++ config/index.js
@@ -1,18 +1,18 @@
-const {
+import {
     createPool
-} = require("mysql2");
-
-let ConnectionList = createPool({
-    host: process.env.hstDb,
-    password: process.env.pswrdDb,
+} from "mysql2"
+import 'dotenv/config'
+let connection = createPool({
+    host: process.env.hostDb,
+    password: process.env.pwdDb,
     user: process.env.userDb,
     database: process.env.dbName,
     multipleStatements: true,
     connectionLimit: 30
 })
 
-ConnectionList.on('connection', (err) => {
-    if (err) throw new Error('couldnt connect to the database, please try again later')
+connection.on('connection', (pool) => {
+    if (!pool) throw new Error('couldnt connect to the database, please try again later')
 })
 
 
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express'
 import path from 'path'
+import {
+    connection as db
+} from './config/index.js'
+// import { urlencoded } from 'body-parser'
 
 //CREATE AN EXPRESS APP
 const app = express()
@@ -7,9 +11,70 @@ const port = +process.env.PORT || 4000
 const router = express.Router()
 
 //MIDDLEWARE
-app.use(router, express.static('./static'))
+app.use(router, express.static('./static'), express.json(), express.urlencoded({
+    extended: true
+}))
 
 //ENDPOINT
 router.get('^/$|/eShop', (req, res) => {
     res.status(200).sendFile(path.resolve('./static/html/index.html'))
+})
+
+router.get('/users', (req, res) => {
+    try {
+        const strQry = `
+            select firstName, lastName, age, emailAdd
+            from Users;
+            `
+        db.query(strQry, (err, results) => {
+            `Unable to fetch all users`
+            if (err) throw new Error(err);
+            res.json({
+                status: res.statusCode,
+                results
+            })
+        })
+    } catch (e) {
+        res.json({
+            status: 404,
+            msg: e.message
+        })
+        console.log('nope');
+    }
+})
+
+router.get('/user/:id', (req, res) => {
+    try {
+        const strQry = `
+            select userID, firstName, lastName, age, emailAdd
+            from Users WHERE userID = ${req.params.id};
+            `
+
+        db.query(strQry, (err, result) => {
+            `Unable to fetch user`
+            if (err) throw new Error(err);
+            res.json({
+                status: res.statusCode,
+                result: result[0]
+            })
+        })
+
+        db.query
+    } catch (e) {
+        res.json({
+            status: 404,
+            msg: e.message
+        })
+    }
+})
+
+router.get('*', (req, res ) => {
+    res.json({
+        status: 404,
+        msg: 'resouce not found'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
 })
